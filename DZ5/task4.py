d0e446a49f692fffb0476b0cd232e59b62f9a69b
OlhaPlesovskykh <74625022+OlhaPlesovskykh@users.noreply.github.com>
2022-12-16 23:06:02 +0200

    Update task4.py

--- DZ5/task4.py
+++ DZ5/task4.py
@@ -2,7 +2,6 @@
 Вывести треугольник #4 с шириной N с помощью цикла while
 """
 
-
 number = 1
 width = int(input("Введите ширину треугольника - "))
 space = width - 1
@@ -11,5 +10,6 @@ while True:
     print(" " * space + "*" * number)
     number += 1
     space -= 1
+    
     if number > width:
         break
