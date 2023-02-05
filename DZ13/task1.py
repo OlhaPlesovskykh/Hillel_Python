197f7c717d2200cb34361f0f88ad3f9534d7a8df
Ольга Синицкая <s.olga.olegovna7@gmail.com>
2023-02-05 15:13:48 +0200

    Done HW13

---
+++ DZ13/task1.py
@@ -0,0 +1,35 @@
+"""
+Напишите функцию change(lst), которая принимает список и меняет местами его первый и последний элемент. В исходном списке минимум 2 элемента.
+Напишите функцию to_dict(lst), которая принимает аргумент в виде списка и возвращает словарь, в котором каждый элемент списка является и ключом и значением. Предполагается,
+что элементы списка будут соответствовать правилам задания ключей в словарях.
+Напишите функцию sum_range(start, end), которая суммирует все целые числа от значения «start» до величины «end» включительно. Если пользователь задаст первое число большее чем второе,
+просто поменяйте их местами.
+"""
+
+
+def change(current_list: list) -> list:
+    if len(current_list) > 1:
+        current_list[-1], current_list[0] = current_list[0], current_list[-1]
+    return current_list
+
+
+def to_dict(current_list: list) -> dict:
+    current_dict = {}
+    for i in current_list:
+        current_dict[i] = i
+    return current_dict
+
+
+def sum_range(start: int, end: int) -> int:
+    number_sum = 0
+    if start > end:
+        start, end = end, start
+    for i in range(start, end + 1):
+        number_sum += i
+    return number_sum
+
+
+list_1 = [1, 2, 3]
+print(change(list_1))
+print(to_dict(list_1))
+print(sum_range(1, 5))
