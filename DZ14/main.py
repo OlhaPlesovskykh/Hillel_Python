06a6e0fc3f908a00fd77ab65cf017e25586969d9
Ольга Синицкая <s.olga.olegovna7@gmail.com>
2023-02-22 12:52:04 +0200

    Fix issue

--- DZ14/main.py
+++ DZ14/main.py
@@ -8,14 +8,15 @@ def cache_decorator(func):
     :param func: Функция
     :return: Результат
     """
-    test = {}
-    test[func.__name__] = {}
-    def inner(*args):
-        for k, v in test.items():
-            if args in v.keys():
-                return v[args]
-        result = func(*args)
-        test[func.__name__][args] = result
+    test = {func.__name__: {}}
+
+    def inner(*args, **kwargs):
+        kwargs.update(zip(func.__code__.co_varnames, args))
+        sorted_tuple = tuple(sorted(kwargs.values(), key=lambda x: x))
+        if sorted_tuple in test[func.__name__]:
+            return test[func.__name__][sorted_tuple]
+        result = func(**kwargs)
+        test[func.__name__][sorted_tuple] = result
         return result
     return inner
 
@@ -43,9 +44,20 @@ def circle_area(r: float) -> float:
     return 3.14 * (r * r)
 
 
-print("Результат выполнения triangle_area(5, 10):", triangle_area(5, 10))
-print("Результат выполнения triangle_area(5, 10):", triangle_area(5, 10))
+print("Результат выполнения triangle_area(5, 10):", triangle_area(a=5, b=10))
+print("Результат выполнения triangle_area(5, 10):", triangle_area(b=10, a=5))
 print("Результат выполнения circle_area(20):", circle_area(20))
 print("Результат выполнения circle_area(20):", circle_area(20))
 print("Результат выполнения triangle_area(10, 10):", triangle_area(10, 10))
 print("Результат выполнения triangle_area(5, 10):", triangle_area(5, 10))
+"""
+Вызвана функция triangle_area с аргументами 5 и 10
+Результат выполнения triangle_area(5, 10): 50
+Результат выполнения triangle_area(5, 10): 50
+Вызвана функция circle_area с аргументом 20
+Результат выполнения circle_area(20): 1256.0
+Результат выполнения circle_area(20): 1256.0
+Вызвана функция triangle_area с аргументами 10 и 10
+Результат выполнения triangle_area(10, 10): 100
+Результат выполнения triangle_area(5, 10): 50
+"""
